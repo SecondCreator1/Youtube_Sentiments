59d905c919c79af66e6fdf1a3f14142d239250b2
SecondCreator1 <aamiraoussama7@gmail.com>
2025-12-29 16:44:18 +0100

    refactor: improve code structure and readability in popup.js

--- yt-chrome-plugin-frontend/popup.js
+++ yt-chrome-plugin-frontend/popup.js
@@ -2,7 +2,7 @@
 
 document.addEventListener("DOMContentLoaded", async () => {
   const outputDiv = document.getElementById("output");
-  const API_KEY = 'AIzaSyC2XoF1pss30wTnv6kCYKQ0qmc0ts3oPIQ';  // Replace with your actual YouTube Data API key 
+  const API_KEY = 'YOUR_YOUTUBE_API_KEY';  // Replace with your actual YouTube Data API key 
   const API_URL = 'http://localhost:5000/';
 
   // Get the current tab's URL
